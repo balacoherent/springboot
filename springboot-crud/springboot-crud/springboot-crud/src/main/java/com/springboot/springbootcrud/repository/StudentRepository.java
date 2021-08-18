00d21d68be851440cf1fe8da5f0cdc29a90889c3
balacoherent <86341262+balacoherent@users.noreply.github.com>
2021-08-18 17:09:26 +0530

    Delete springboot-crud/springboot-crud directory

--- springboot-crud/springboot-crud/springboot-crud/src/main/java/com/springboot/springbootcrud/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.springbootcrud.repository;
-
-import com.springboot.springbootcrud.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-
-}
-
